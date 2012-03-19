1d23c3777e2445f4d1b0d7f7e24fabe6f1f73c45
Kostas Papadimitriou <kpap@grnet.gr>
2012-03-19 19:06:57 +0200

    Add MEDIA_URL in response context

--- snf-pithos-web-client/pithos_web_client/views.py
+++ snf-pithos-web-client/pithos_web_client/views.py
@@ -1,18 +1,18 @@
 # Copyright 2011-2012 GRNET S.A. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or
 # without modification, are permitted provided that the following
 # conditions are met:
-# 
+#
 #   1. Redistributions of source code must retain the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer.
-# 
+#
 #   2. Redistributions in binary form must reproduce the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer in the documentation and/or other materials
 #      provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
 # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -25,7 +25,7 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # The views and conclusions contained in the software and
 # documentation are those of the authors and should not be
 # interpreted as representing official policies, either expressed
@@ -34,8 +34,12 @@
 from django.shortcuts import render_to_response
 
 from pithos_web_client import settings
+from django.conf import settings as django_settings
 
+MEDIA_URL = getattr(settings, "PITHOS_WEB_CLIENT_MEDIA_URL", \
+        getattr(django_settings, "MEDIA_URL", "/static/"))
 
 def index(request):
-    return render_to_response('pithos_web_client/index.html', {'settings': settings})
+    return render_to_response('pithos_web_client/index.html', \
+            {'settings': settings, 'MEDIA_URL': MEDIA_URL})
 
