2c313efae5573484e0111e8393d098bb99ee5488
Kostas Papadimitriou <kpap@grnet.gr>
2012-03-30 12:57:45 +0300

    No need for make.sh, build takes place in setup.py

--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ant
-rm -rf snf-pithos-web-client/pithos_web_client/static/pithos_web_client/*
-cp -r bin/www/gr.grnet.pithos.web.Pithos/* snf-pithos-web-client/pithos_web_client/static/pithos_web_client
-rm -rf snf-pithos-web-client/pithos_web_client/templates/pithos_web_client/index.html
-cp bin/www/gr.grnet.pithos.web.Pithos/index.html snf-pithos-web-client/pithos_web_client/templates/pithos_web_client/index.html
