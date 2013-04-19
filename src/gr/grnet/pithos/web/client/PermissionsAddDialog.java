6e804d92981ae58ad7354adba21983be5a8b7c9f
Christos KK Loverdos <loverdos@gmail.com>
2013-04-19 15:30:44 +0300

    Fix a typo in File/Share/Add Group dialog
    The column header for groups should be "Group" not "User"
    
    Refs #3575

--- src/gr/grnet/pithos/web/client/PermissionsAddDialog.java
+++ src/gr/grnet/pithos/web/client/PermissionsAddDialog.java
@@ -88,7 +88,7 @@ public class PermissionsAddDialog extends DialogBox {
         inner.addStyleName("inner");
 
         final FlexTable permTable = new FlexTable();
-        permTable.setText(0, 0, isUser ? Const.TXT_USER : Const.TXT_USER);
+        permTable.setText(0, 0, isUser ? Const.TXT_USER : Const.TXT_GROUP);
         permTable.setText(0, 1, "Read Only");
         permTable.setText(0, 2, "Read/Write");
         permTable.getFlexCellFormatter().setStyleName(0, 0, "props-toplabels");
