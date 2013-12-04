48ac22beed68e3e2c313872cb5b6a54ec896e398
Christos KK Loverdos <loverdos@gmail.com>
2013-12-04 16:38:14 +0200

    Update rsync script to reflect new dev environment

--- rsync-to-dev.sh
+++ rsync-to-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync --delete -avz ./out/artifacts/pithos_web_client_GWT/gr.grnet.pithos.web.Pithos/* root@pithos.synnefo.dev.grnet.gr:/usr/share/synnefo/static/pithos_webclient
+rsync --delete -avz ./out/artifacts/pithos_web_client_GWT/gr.grnet.pithos.web.Pithos/* root@pithos.synnefo.live:/usr/share/synnefo/static/pithos_webclient
